e0a9040cf5107449e61ef1606dd9e5b37800dc9f
mpatnode1 <149105967+mpatnode1@users.noreply.github.com>
2024-12-17 08:45:48 -0600

    Uploaded Build V.0.2.

--- ADVGSE_Final/Assets/Scripts/LevelBlock.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class LevelBlock : MonoBehaviour
-{
-    private void FixedUpdate()
-    {
-        transform.position += Vector3.back * Time.deltaTime * LevelManager.Instance.levelMovementSpeed;
-    }
-}
